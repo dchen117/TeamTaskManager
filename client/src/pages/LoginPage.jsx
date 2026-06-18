c6a800130119c8e947f0e7944a18cd2b7cbae171
Daniel Chen <dchen117@terpmail.umd.edu>
2026-06-18 19:45:40 -0400

    replaced old login and registration pages with ones from shadcn login, and added home page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,42 +1,10 @@
-import { useState } from "react";
-import AuthForm from "../components/AuthForm.jsx";
-import { authFields } from "../components/AuthFields.js";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../services/AuthContext.jsx";
-
-const loginFields = [
-  authFields.email,
-  authFields.password,
-];
-
-export default function Login() {
-  const [values, setValues] = useState({});
-  const navigate = useNavigate();
-  const { login } = useContext(AuthContext);
-
-  function handleChange(e) {
-    setValues({
-      ...values,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    await login(values.email, values.password);
-    console.log('login successful');
-    navigate('/home');
-  }
-
+import { LoginForm } from "@/components/login-form"
+export default function LoginPage() {
   return (
-    <AuthForm
-      fields={loginFields}
-      values={values}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-      submitLabel="Login"
-      message={<p>No account? <a href="/register">Sign up!</a></p>}
-    />
-  );
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -1,45 +1,11 @@
-import { useState } from "react";
-import AuthForm from "../components/AuthForm.jsx";
-import { authFields } from "../components/AuthFields.js";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../services/AuthContext.jsx";
+import { SignupForm } from "@/components/signup-form"
 
-const registrationFields = [
-  authFields.displayname,
-  authFields.username,
-  authFields.email,
-  authFields.password,
-  authFields.confirmPassword,
-];
-
-export default function Registration() {
-  const [values, setValues] = useState({});
-  const navigate = useNavigate();
-  const { register } = useContext(AuthContext);
-
-  function handleChange(e) {
-    setValues({
-      ...values,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    await register(values.displayname, values.username, values.email, values.password, values.confirmPassword);
-    console.log('Registration successful');
-    navigate('/home');
-  }
-
+export default function Page() {
   return (
-    <AuthForm
-      fields={registrationFields}
-      values={values}
-      onChange={handleChange}
-      onSubmit={handleSubmit}
-      submitLabel="Register"
-      message={<p>Already have an account? <a href="/">Sign in!</a></p>}
-    />
-  );
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  )
 }
