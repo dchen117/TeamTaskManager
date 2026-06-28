27d89508eb5ff5b420cd4a3bbdac17884311690c
Daniel Chen <dchen117@terpmail.umd.edu>
2026-06-28 13:41:29 -0400

    added empty dashboard to homepage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -13,6 +13,7 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { EmptyDemo } from "@/components/no-projects"
 
 export default function Page() {
   return (
@@ -41,14 +42,15 @@ export default function Page() {
             </Breadcrumb>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {/* <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
           </div>
           <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div>
+        </div> */}
+        <EmptyDemo />
       </SidebarInset>
     </SidebarProvider>
   )
