9a1634b5325312118ab8d75b9b22d0775b3c7743
Daniel Chen <dchen117@terpmail.umd.edu>
2026-07-23 11:37:31 -0400

    added services for creating and updating tasks, and updating statuses

--- client/src/services/tasks.js
+++ client/src/services/tasks.js
@@ -1,8 +1,34 @@
 import api from '../services/api.js';
 
-export async function getTasks(projectId) {
+async function getTasks(projectId) {
   const res = await api.get(
-    `/projects/${projectId}/tasks`
+    `/projects/${projectId}/get-tasks`
   );
   return res.data;
 }
+
+async function createTask({ projectId, data }) {
+  const res = await api.post(
+    `/projects/${projectId}/create-task`,
+    data,
+  );
+  return res.data;
+}
+
+async function updateStatus({ projectId, statusId, data }) {
+  const res = await api.put(
+    `/projects/${projectId}/${statusId}/update-status`,
+    data,
+  );
+  return res;
+}
+
+async function updateTask({ taskId, data }) {
+  const res = await api.put(
+    `/tasks/${taskId}/update-task`,
+    data
+  );
+  return res.data;
+}
+
+export { getTasks, createTask, updateTask, updateStatus };
