0a8aac7bf379a52b0e255cb3eceb9b6e34938f80
Daniel Chen <dchen117@terpmail.umd.edu>
2026-07-23 11:29:08 -0400

    updateStatusMutation sets cached data to reflect updates in statuses

--- client/src/hooks/useProjects.js
+++ client/src/hooks/useProjects.js
@@ -1,14 +1,26 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { getProjects, createProject } from "../services/projects";
+import { updateStatus } from "@/services/tasks";
 
 export function useProjects(workspaceId) {
   const queryClient = useQueryClient();
+
+  // add an id field for dnd-kit's move helper
+  const selectProjects = (projects) =>
+      projects.map(project => ({
+        ...project,
+        statuses: (project.statuses ?? []).map(status => ({
+          ...status,
+          id: status._id,
+        })),
+      }))
 
   // READ
   const projectsQuery = useQuery({
     queryKey: ["projects", workspaceId],
     queryFn: () => getProjects(workspaceId),
     enabled: !!workspaceId,
+    select: selectProjects
   });
 
   // WRITE
@@ -21,9 +33,25 @@ export function useProjects(workspaceId) {
     }
   });
 
+  const updateStatusMutation = useMutation({
+    mutationFn: updateStatus,
+    onSuccess: (updatedProject, variables) => {
+      queryClient.setQueryData(
+        ["projects", variables.workspaceId],
+        projects =>
+          projects?.map(project =>
+            project._id === updatedProject._id
+              ? updatedProject
+              : project
+          )
+      );
+    }
+  })
+
   return {
     ...projectsQuery,
     createProject: createProjectMutation.mutate,
-    isCreating: createProjectMutation.isPending
+    isCreating: createProjectMutation.isPending,
+    updateStatus: updateStatusMutation.mutate
   }
 }
