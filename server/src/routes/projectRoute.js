3a30e408b5f966cc9d6ed9846b9fceec459dc788
Daniel Chen <dchen117@terpmail.umd.edu>
2026-08-12 12:53:16 -0400

    updated projectRoute

--- server/src/routes/projectRoute.js
+++ server/src/routes/projectRoute.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import { createProject, deleteProject, updateStatus } from '../controllers/projectController.js';
+import { createProject, deleteProject, updateProject } from '../controllers/projectController.js';
 import { authenticate, ROLES, requireRole } from '../controllers/authController.js';
 import { createTask, getTasks } from '../controllers/taskController.js';
+import { getStatuses, createStatus } from '../controllers/statusController.js'
 const router = express.Router();
 
 router.use(authenticate); // Protect all project routes
-router.delete('/:projectId/delete-project', authenticate, requireRole(ROLES.ADMIN), deleteProject);
-router.post('/:projectId/create-task', createTask);
+// router.delete('/:projectId/delete-project', authenticate, requireRole(ROLES.ADMIN), deleteProject);
+router.delete('/:projectId/delete-project', deleteProject);
+router.put('/:projectId/update-project', updateProject)
 router.get('/:projectId/get-tasks', getTasks);
-router.put('/:projectId/:statusId/update-status', updateStatus);
+router.post('/:projectId/create-task', createTask);
+router.get('/:projectId/get-statuses', getStatuses)
+router.post('/:projectId/create-status', createStatus);
 
 export default router;
