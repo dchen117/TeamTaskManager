25a9af4408adf4e4d87e98cf95585ed553d4c0cb
Daniel Chen <dchen117@terpmail.umd.edu>
2026-08-12 14:02:35 -0400

    tasks can be edited

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -4,7 +4,7 @@ import {
     CardTitle,
     CardContent,
 } from '@/components/ui/card';
-import { MoreHorizontalIcon } from 'lucide-react';
+import { MoreHorizontalIcon, Trash2Icon, Edit2Icon } from 'lucide-react';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -12,38 +12,73 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useSortable } from '@dnd-kit/react/sortable';
+import { DeleteDialog } from './delete-dialog';
+import { useState } from 'react';
+import { useTasks } from '@/hooks/useTasks';
+import { useParams } from 'react-router-dom';
+import { EditTaskSheet } from './edit-task-sheet';
 
-function TaskCard({ task, column, index }) {
+function TaskCard({ task, statusId, index }) {
     const { ref, isDragging } = useSortable({
         id: task._id,
         index: index,
         type: 'task',
         accept: 'task',
-        group: column
+        group: statusId
     });
+    const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
+    const [editTaskOpen, setEditTaskOpen] = useState(false);
+    const { projectId } = useParams();
+    const { deleteTask } = useTasks(projectId);
+
+    function handleDelete(e) {
+        e.preventDefault()
+        deleteTask(task._id)
+        setDeleteDialogOpen(false)
+    }
 
     return (
-        <Card className="group w-full text-left" ref={ref} data-dragging={isDragging}>
-            <CardHeader>
-                <CardTitle className="flex items-center justify-between gap-2">
-                    {task.title}
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <button className="p-1 rounded hover:bg-muted/50 opacity-0 group-hover:opacity-100">
-                                <MoreHorizontalIcon className="size-6 text-muted-foreground" />
-                            </button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent side="bottom" align="end" className="w-40">
-                            <DropdownMenuItem>Edit Task</DropdownMenuItem>
-                            <DropdownMenuItem>Delete Task</DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </CardTitle>
-            </CardHeader>
-            <CardContent>
-                <p>{task.description}</p>
-            </CardContent>
-        </Card>
+        <>
+            <Card className="group w-full text-left" ref={ref} data-dragging={isDragging}>
+                <CardHeader>
+                    <CardTitle className="flex items-center justify-between gap-2">
+                        {task.title}
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <button className="p-1 rounded hover:bg-muted/50 opacity-0 group-hover:opacity-100">
+                                    <MoreHorizontalIcon className="size-6 text-muted-foreground" />
+                                </button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent side="bottom" align="end" className="w-40">
+                                <DropdownMenuItem onSelect={() => setEditTaskOpen(true)}>
+                                    <Edit2Icon />
+                                    <span>Edit Task</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem variant="destructive" onSelect={() => setDeleteDialogOpen(true)}>
+                                    <Trash2Icon />
+                                    <span>Delete Task</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <p>{task.description}</p>
+                </CardContent>
+            </Card>
+            <DeleteDialog 
+                open={deleteDialogOpen}
+                onOpenChange={setDeleteDialogOpen}
+                title="Delete Task?" 
+                description="Are you sure you want to remove this task?" 
+                handleSubmit={handleDelete} 
+            />
+            <EditTaskSheet
+                open={editTaskOpen}
+                onOpenChange={setEditTaskOpen}
+                task={task}
+            />
+        </>
     );
 }
 
