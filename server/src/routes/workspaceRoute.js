a81807f60391ec31107b3c39f9c29e2c6ec0ea33
Daniel Chen <dchen117@terpmail.umd.edu>
2026-08-12 12:54:10 -0400

    updated workspaceRoute

--- server/src/routes/workspaceRoute.js
+++ server/src/routes/workspaceRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getWorkspaces, createWorkspace, joinWorkspace, leaveWorkspace } from '../controllers/workspaceController.js';
-import { authenticate } from '../controllers/authController.js';
+import { getWorkspaces, createWorkspace, updateWorkspace, joinWorkspace, leaveWorkspace, deleteWorkspace } from '../controllers/workspaceController.js';
+import { authenticate, checkWorkspaceAccess } from '../controllers/authController.js';
 import { getProjects, createProject } from '../controllers/projectController.js';
 const router = express.Router();
 
@@ -8,7 +8,10 @@ router.use(authenticate); // Protect all workspace routes
 router.get('/get-workspaces', getWorkspaces);
 router.post('/create-workspace', createWorkspace);
 router.post('/join-workspace', joinWorkspace);
-router.get('/:workspaceId/leave-workspace', leaveWorkspace);
+router.use('/:workspaceId', checkWorkspaceAccess);
+router.put('/:workspaceId/update-workspace', updateWorkspace);
+router.delete('/:workspaceId/leave-workspace', leaveWorkspace);
+router.delete('/:workspaceId/delete-workspace', deleteWorkspace);
 router.post('/:workspaceId/create-project', createProject);
 router.get('/:workspaceId/get-projects', getProjects);
 
