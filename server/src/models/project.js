1e5234b7dd171934919c4207a44e58addf9ff277
Daniel Chen <dchen117@terpmail.umd.edu>
2026-08-12 12:51:33 -0400

    removed default statuses from project model

--- server/src/models/project.js
+++ server/src/models/project.js
@@ -9,12 +9,6 @@ const projectSchema = new mongoose.Schema({
     workspace: { type: mongoose.Schema.Types.ObjectId, ref: 'Workspace', required: true },
     createdAt: { type: Date, default: Date.now },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    statuses: {
-        type: [{ 
-            name: { type: String, required: true },
-            order: { type: String, required: true }
-        }],
-    }
 });
 
 export default mongoose.model('Project', projectSchema);
