ef825e6ca04348ecd2a5ca3dde03ca5b073e9d94
Daniel Chen <dchen117@terpmail.umd.edu>
2026-08-12 12:46:24 -0400

    added default statuses to created projects

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -1,4 +1,7 @@
-import Project from '../models/project.js'
+import Project from '../models/project.js';
+import Status from '../models/status.js';
+import Task from '../models/task.js'
+import mongoose from 'mongoose';
 
 async function getProjects(req, res) {
     const { workspaceId } = req.params;
@@ -14,23 +17,48 @@ async function createProject(req, res) {
     const { name, description } = req.body;
     const { workspaceId } = req.params;
     const userId = req.userId;
+    const session = await mongoose.startSession();
     try {
-        const project = await Project.create({ name, description, workspace: workspaceId, createdBy: userId });
+        let project;
+        await session.withTransaction(async () => { // Project is created with default statuses atomically
+            [project] = await Project.create([{ name, description, workspace: workspaceId, createdBy: userId }], { session });
+            const statuses = [
+                { project: project._id, name: "To Do", order: 'a0' },
+                { project: project._id, name: "In Progress", order: 'a1' },
+                { project: project._id, name: "Done", order: 'a2' }
+            ]
+            await Status.insertMany(statuses, { session })
+        })
         res.status(201).json({ message: 'Project created successfully', project });
     } catch (error) {
         res.status(500).json({ error: error.message });
+    } finally {
+        await session.endSession();
     }
 }
 
 async function deleteProject(req, res) {
     const { projectId } = req.params;
     const userId = req.userId;
+    const session = await mongoose.startSession();
     try {
-        await Project.deleteOne({ _id: projectId, createdBy: userId });
+        await Task.deleteMany({ project: projectId }, { session });
+        await Status.deleteMany({ project: projectId }, { session });
+        await Project.deleteOne({ _id: projectId, createdBy: userId }, { session });
         res.status(200).json({ message: 'Project deleted successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
+    } finally {
+        await session.endSession();
     }
 }
 
-export { getProjects, createProject, deleteProject }
+function updateProject(req, res) {
+    const { projectId } = req.params;
+    const { name } = req.body;
+    Project.findByIdAndUpdate(projectId, { name }, { returnDocument: 'after' })
+        .then(project => res.json(project))
+        .catch(error => res.status(500).json({ error: error.message }));
+}
+
+export { getProjects, updateProject, createProject, deleteProject }
